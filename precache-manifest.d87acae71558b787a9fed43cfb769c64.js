10f3b413b0d95aee26039d906b77c31284740ade
Festus Ebin <ebinfestus@gmail.com>
2020-03-05 18:09:04 +0100

    Updates

---
+++ precache-manifest.d87acae71558b787a9fed43cfb769c64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1984dc5df36a0b6bb20e01d720e5563",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "407722be99fc8d1c74df",
+    "url": "/react-demo/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "cb05b07fd1f5c8a95b83",
+    "url": "/react-demo/static/css/main.c3fdb943.chunk.css"
+  },
+  {
+    "revision": "407722be99fc8d1c74df",
+    "url": "/react-demo/static/js/2.10961848.chunk.js"
+  },
+  {
+    "revision": "ea4b5ae141dc8c71e175204f6e8e684d",
+    "url": "/react-demo/static/js/2.10961848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb05b07fd1f5c8a95b83",
+    "url": "/react-demo/static/js/main.ebec5c2c.chunk.js"
+  },
+  {
+    "revision": "0c636ea9487d798002c4",
+    "url": "/react-demo/static/js/runtime-main.fe745fde.js"
+  }
+]);
